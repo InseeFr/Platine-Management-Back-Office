824d98337b2d37399ac34c507b03ba23fa1aa57e
y72wvh <betty.becuwe@insee.fr>
2024-06-21 13:55:16 +0200

    fix: email regex

--- src/main/java/fr/insee/survey/datacollectionmanagement/util/EmailValidator.java
+++ src/main/java/fr/insee/survey/datacollectionmanagement/util/EmailValidator.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailValidator {
-    private static final String EMAIL_REGEX = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+    private static final String EMAIL_REGEX = "^(?=.{1,64}@)[A-Za-z0-9_-]+(?:\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(?:\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
 
     public static boolean isValidEmail(String email) {
         // Compiler l'expression régulière
