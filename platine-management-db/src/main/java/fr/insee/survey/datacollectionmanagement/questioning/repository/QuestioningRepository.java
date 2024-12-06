a22798e9f18df9bcb2f3e5e105f4753c1bc97444
Betty Becuwe <betty.becuwe@insee.fr>
2024-12-06 17:18:16 +0100

    feat: improve search questioning using union all

--- platine-management-db/src/main/java/fr/insee/survey/datacollectionmanagement/questioning/repository/QuestioningRepository.java
+++ platine-management-db/src/main/java/fr/insee/survey/datacollectionmanagement/questioning/repository/QuestioningRepository.java
@@ -16,13 +16,23 @@ public interface QuestioningRepository extends JpaRepository<Questioning, Long> 
                                                       String surveyUnitIdSu);
 
     @Query("""
-    SELECT q FROM Questioning q
-        LEFT JOIN FETCH q.questioningAccreditations acc
-        LEFT JOIN FETCH q.questioningEvents evt
-        LEFT JOIN FETCH q.questioningCommunications comm
-    WHERE q.surveyUnit.idSu =:searchParam 
-        OR q.surveyUnit.identificationName =:searchParam 
-        OR acc.idContact =:searchParam
+                SELECT q FROM Questioning q
+                    LEFT JOIN FETCH q.questioningAccreditations acc
+                    LEFT JOIN FETCH q.questioningEvents evt
+                    LEFT JOIN FETCH q.questioningCommunications comm
+                WHERE q.surveyUnit.idSu = :searchParam
+                UNION ALL
+                SELECT q FROM Questioning q
+                    LEFT JOIN FETCH q.questioningAccreditations acc
+                    LEFT JOIN FETCH q.questioningEvents evt
+                    LEFT JOIN FETCH q.questioningCommunications comm
+                WHERE q.surveyUnit.identificationName = :searchParam
+                UNION ALL
+                SELECT q FROM Questioning q
+                    LEFT JOIN FETCH q.questioningAccreditations acc
+                    LEFT JOIN FETCH q.questioningEvents evt
+                    LEFT JOIN FETCH q.questioningCommunications comm
+                WHERE acc.idContact = :searchParam
     """)
 
     Page<Questioning> findQuestioningByParam(String searchParam,Pageable pageable );
