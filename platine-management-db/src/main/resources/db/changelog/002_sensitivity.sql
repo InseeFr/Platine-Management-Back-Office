77fcc384491bf51fcde29952b7d415e0f53e247a
davdarras <david.darras@insee.fr>
2025-02-17 15:34:26 +0100

    feat: add liquibase and demo data

---
+++ platine-management-db/src/main/resources/db/changelog/002_sensitivity.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+
+--changeset davdarras:002-01
+
+ALTER TABLE public.campaign ADD COLUMN datacollection_target varchar(255) NULL;
+ALTER TABLE public.campaign ADD COLUMN sensitivity bool DEFAULT false NULL;
+ALTER TABLE public.campaign ADD CONSTRAINT campaign_datacollection_target_check CHECK (datacollection_target::text = ANY (ARRAY['LUNATIC_SENSITIVE', 'LUNATIC_NORMAL', 'XFORM1', 'XFORM2']::text[]));
+
+--changeset davdarras:002-02
+
+UPDATE public.campaign set datacollection_target='LUNATIC_NORMAL';
+UPDATE public.campaign set sensitivity=false;
