dc2789b59ff61cd45b3c73cb977927b9668618c9
Betty Becuwe <77614323+BettyB979@users.noreply.github.com>
2025-02-19 16:13:36 +0100

    fix: anonymous config with admin role (#117)
    * fix: anonylous config with admin role
    
    * fix: remane to noauth security context

--- platine-management-shared/src/main/java/fr/insee/survey/datacollectionmanagement/constants/AuthConstants.java
+++ platine-management-shared/src/main/java/fr/insee/survey/datacollectionmanagement/constants/AuthConstants.java
@@ -3,6 +3,7 @@ package fr.insee.survey.datacollectionmanagement.constants;
 public class AuthConstants {
 
     public static final String OIDC = "OIDC";
+    public static final String NOAUTH = "noauth";
 
     private AuthConstants() {
         throw new IllegalStateException("Utility class");
