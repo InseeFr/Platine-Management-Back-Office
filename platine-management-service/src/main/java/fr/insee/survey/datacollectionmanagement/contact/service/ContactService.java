61d87583c9f652ccb07192b0a5c848038cfd5e16
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2025-02-27 16:53:11 +0100

    feat: add firstName et lastName to list contacts in questioningDetailsDto

--- platine-management-service/src/main/java/fr/insee/survey/datacollectionmanagement/contact/service/ContactService.java
+++ platine-management-service/src/main/java/fr/insee/survey/datacollectionmanagement/contact/service/ContactService.java
@@ -6,6 +6,7 @@ import fr.insee.survey.datacollectionmanagement.contact.dto.ContactDetailsDto;
 import fr.insee.survey.datacollectionmanagement.contact.dto.ContactDto;
 import fr.insee.survey.datacollectionmanagement.contact.dto.SearchContactDto;
 import fr.insee.survey.datacollectionmanagement.exception.NotFoundException;
+import fr.insee.survey.datacollectionmanagement.query.dto.QuestioningContactDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -32,6 +33,14 @@ public interface ContactService {
      * @return contact found
      */
     Contact findByIdentifier(String identifier);
+
+    /**
+     * Find all contacts by identifiers list
+     *
+     * @param identifiers
+     * @return
+     */
+    List<QuestioningContactDto> findByIdentifiers(List<String> identifiers);
 
     /**
      * Update an existing contact and its address, or creates a new one
---
+++ platine-management-shared/src/main/java/fr/insee/survey/datacollectionmanagement/query/dto/QuestioningContactDto.java
@@ -0,0 +1,4 @@
+package fr.insee.survey.datacollectionmanagement.query.dto;
+
+public record QuestioningContactDto(String identifier, String lastName, String firstName) {
+}
