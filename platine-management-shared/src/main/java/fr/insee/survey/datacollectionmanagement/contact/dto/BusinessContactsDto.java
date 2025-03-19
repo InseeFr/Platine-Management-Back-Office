8d9c637fff672525cde4a20e9a5c493f9bdc739b
Betty Becuwe <77614323+BettyB979@users.noreply.github.com>
2025-03-19 14:36:19 +0100

    fix: change name for business endpoint for businessContactDtoList (#130)

--- platine-management-shared/src/main/java/fr/insee/survey/datacollectionmanagement/contact/dto/BusinessContactsDto.java
+++ platine-management-shared/src/main/java/fr/insee/survey/datacollectionmanagement/contact/dto/BusinessContactsDto.java
@@ -1,5 +1,6 @@
 package fr.insee.survey.datacollectionmanagement.contact.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,5 +12,6 @@ public class BusinessContactsDto {
     private int hit;
     private int start;
     private int count;
+    @JsonProperty("contact")
     private List<BusinessContactDto> businessContactDtoList;
 }
