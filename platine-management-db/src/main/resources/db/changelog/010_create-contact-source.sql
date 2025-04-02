e3060401bdf34e105080797e634270dfdf528fb0
davdarras <david.darras@insee.fr>
2025-04-02 18:38:39 +0200

    feat: add table contact_source

---
+++ platine-management-db/src/main/resources/db/changelog/010_create-contact-source.sql
@@ -0,0 +1,17 @@
+--liquibase formatted sql
+
+--changeset davdarras:010-01
+CREATE TABLE public.contact_source (
+    source_id VARCHAR(255) NOT NULL,
+    survey_unit_id VARCHAR(255) NOT NULL,
+    contact_id VARCHAR(255) NOT NULL,
+    is_main BOOLEAN NOT NULL,
+    CONSTRAINT contact_source_pkey PRIMARY KEY (source_id, survey_unit_id, contact_id),
+    CONSTRAINT fk_contact_source_source FOREIGN KEY (source_id)
+        REFERENCES public."source"(id) ON DELETE CASCADE,
+    CONSTRAINT fk_contact_source_survey_unit FOREIGN KEY (survey_unit_id)
+        REFERENCES public.survey_unit(id_su) ON DELETE CASCADE,
+    CONSTRAINT fk_contact_source_contact FOREIGN KEY (contact_id)
+        REFERENCES public.contact(identifier) ON DELETE CASCADE,
+    INDEX idx_contact_source_source_survey (source_id, survey_unit_id)
+);
