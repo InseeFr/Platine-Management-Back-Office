3ec2b4e1114bd6ad0e41baf936be1a8146bcbc2d
Betty Becuwe <betty.becuwe@insee.fr>
2025-04-04 11:46:44 +0200

    test: get metadata business 200 ok

--- platine-management-api/src/test/java/fr/insee/survey/datacollectionmanagement/metadata/controller/MetadataControllerTest.java
+++ platine-management-api/src/test/java/fr/insee/survey/datacollectionmanagement/metadata/controller/MetadataControllerTest.java
@@ -4,6 +4,7 @@ package fr.insee.survey.datacollectionmanagement.metadata.controller;
 import fr.insee.survey.datacollectionmanagement.configuration.AuthenticationUserProvider;
 import fr.insee.survey.datacollectionmanagement.constants.AuthorityRoleEnum;
 import fr.insee.survey.datacollectionmanagement.constants.UrlConstants;
+import fr.insee.survey.datacollectionmanagement.metadata.domain.Campaign;
 import fr.insee.survey.datacollectionmanagement.metadata.service.CampaignService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,14 +12,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -44,6 +49,34 @@ class MetadataControllerTest {
         this.mockMvc.perform(get(UrlConstants.API_METADATA_BUSINESS, campaignId)).andDo(print())
                 .andExpect(status().is(HttpStatus.NOT_FOUND.value()));
     }
-    
+
+    @Test
+    void getCampaignOk() throws Exception {
+        String campaignId = "SOURCE12023T01";
+
+        assertDoesNotThrow(() -> campaignService.findById(campaignId));
+        Campaign campaign = campaignService.findById(campaignId);
+        this.mockMvc.perform(get(UrlConstants.API_METADATA_BUSINESS, campaignId)).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_XML))
+                .andExpect(xpath("/InformationsCollecte/ServiceProducteur/Libelle")
+                        .string(Optional.ofNullable(campaign.getSurvey().getSource().getOwner().getLabel()).orElse("")))
+                .andExpect(xpath("/InformationsCollecte/ServiceProducteur/MinistereTutelle")
+                        .string(Optional.ofNullable(campaign.getSurvey().getSource().getOwner().getMinistry()).orElse("")))
+                .andExpect(xpath("/InformationsCollecte/Enquete/AnneeCollecte")
+                        .string(Optional.ofNullable(String.valueOf(campaign.getSurvey().getYear())).orElse("")))
+                .andExpect(xpath("/InformationsCollecte/Enquete/CaractereObligatoire")
+                        .string("non"))
+                .andExpect(xpath("/InformationsCollecte/Enquete/ObjectifsCourts")
+                        .string(Optional.ofNullable(campaign.getSurvey().getShortObjectives()).orElse("")))
+                .andExpect(xpath("/InformationsCollecte/Enquete/URLDiffusion")
+                        .string(Optional.ofNullable(campaign.getSurvey().getDiffusionUrl()).orElse("")))
+                .andExpect(xpath("/InformationsCollecte/Enquete/URLNotice")
+                        .string(Optional.ofNullable(campaign.getSurvey().getNoticeUrl()).orElse("")))
+                .andExpect(xpath("/InformationsCollecte/Enquete/URLSpecimen")
+                        .string(Optional.ofNullable(campaign.getSurvey().getSpecimenUrl()).orElse("")))
+
+                .andExpect(xpath("/InformationsCollecte/Campagne/Libelle")
+                        .string(Optional.ofNullable(campaign.getCampaignWording()).orElse("")));
+    }
 
 }
