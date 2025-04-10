1f5d20fc6c453b4008402979792ebf8fae3312cb
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2025-04-10 10:26:37 +0200

    fix: removed unused method

--- platine-management-shared/src/main/java/fr/insee/survey/datacollectionmanagement/query/enums/QuestionnaireStatusTypeEnum.java
+++ platine-management-shared/src/main/java/fr/insee/survey/datacollectionmanagement/query/enums/QuestionnaireStatusTypeEnum.java
@@ -8,13 +8,4 @@ public enum QuestionnaireStatusTypeEnum {
     QuestionnaireStatusTypeEnum(String type) {
         value = type;
     }
-
-    public static QuestionnaireStatusTypeEnum fromValue(String value) {
-        for (QuestionnaireStatusTypeEnum param : QuestionnaireStatusTypeEnum.values()) {
-            if (param.value.equalsIgnoreCase(value)) {
-                return param;
-            }
-        }
-        throw new IllegalArgumentException("No constant found for value: " + value);
-    }
 }
