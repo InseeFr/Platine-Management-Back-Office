cb152025cdae14619f55608172c42c40af162256
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2025-05-13 17:34:55 +0200

    feat(wip): init tests

---
+++ platine-management-service/src/test/java/fr/insee/survey/datacollectionmanagement/questioning/service/impl/QuestioningAccreditationServiceImplTest.java
@@ -0,0 +1,38 @@
+package fr.insee.survey.datacollectionmanagement.questioning.service.impl;
+
+import fr.insee.survey.datacollectionmanagement.exception.NotFoundException;
+import fr.insee.survey.datacollectionmanagement.questioning.service.QuestioningAccreditationService;
+import fr.insee.survey.datacollectionmanagement.questioning.service.stub.ContactServiceStub;
+import fr.insee.survey.datacollectionmanagement.questioning.service.stub.QuestioningAccreditationRepositoryStub;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+@ExtendWith(MockitoExtension.class)
+class QuestioningAccreditationServiceImplTest {
+
+    private QuestioningAccreditationService questioningAccreditationService;
+
+    @BeforeEach
+    void init() {
+
+        QuestioningAccreditationRepositoryStub questioningAccreditationRepository = new QuestioningAccreditationRepositoryStub();
+        ContactServiceStub contactService = new ContactServiceStub();
+        questioningAccreditationService = new QuestioningAccreditationServiceImpl(questioningAccreditationRepository, contactService);
+    }
+
+    @Test
+    @DisplayName("Should throw error with unknown contact")
+    void setQuestioningAccreditationToUnknownContact() {
+        Long contactId = Integer.toUnsignedLong(123);
+        assertThrows(NotFoundException.class, () ->
+            questioningAccreditationService.setQuestioningAccreditationToContact("testId", contactId));
+
+    }
+
+}
---
+++ platine-management-service/src/test/java/fr/insee/survey/datacollectionmanagement/questioning/service/stub/ContactServiceStub.java
@@ -0,0 +1,112 @@
+package fr.insee.survey.datacollectionmanagement.questioning.service.stub;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import fr.insee.survey.datacollectionmanagement.contact.domain.Contact;
+import fr.insee.survey.datacollectionmanagement.contact.dto.ContactDetailsDto;
+import fr.insee.survey.datacollectionmanagement.contact.dto.ContactDto;
+import fr.insee.survey.datacollectionmanagement.contact.dto.SearchContactDto;
+import fr.insee.survey.datacollectionmanagement.contact.service.ContactService;
+import fr.insee.survey.datacollectionmanagement.exception.NotFoundException;
+import fr.insee.survey.datacollectionmanagement.query.dto.QuestioningContactDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Map;
+
+public class ContactServiceStub implements ContactService {
+    @Override
+    public Page<Contact> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<Contact> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Contact findByIdentifier(String identifier) {
+        return null;
+    }
+
+    @Override
+    public ContactDto update(ContactDto contactDto, JsonNode payload) {
+        return null;
+    }
+
+    @Override
+    public boolean existsByIdentifier(String identifier) {
+        return false;
+    }
+
+    @Override
+    public List<QuestioningContactDto> findByIdentifiers(Map<String, Boolean> mapContactsIdMain) {
+        return List.of();
+    }
+
+    @Override
+    public Contact saveContact(Contact contact) {
+        return null;
+    }
+
+    @Override
+    public void deleteContact(String identifier) {
+        //not used
+    }
+
+    @Override
+    public Contact updateOrCreateContact(String id, ContactDto contactDto, JsonNode payload) {
+        return null;
+    }
+
+    @Override
+    public Contact createAddressAndEvent(Contact contact, JsonNode payload) {
+        return null;
+    }
+
+    @Override
+    public Contact updateContactAddressEvent(Contact contact, JsonNode payload) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public void deleteContactAddressEvent(Contact contact) {
+        //not used
+    }
+
+    @Override
+    public Page<SearchContactDto> searchContactByIdentifier(String identifier, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Page<SearchContactDto> searchContactByEmail(String email, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Page<SearchContactDto> searchContactByName(String name, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public ContactDto convertToDto(Contact contact) {
+        return null;
+    }
+
+    @Override
+    public Contact convertToEntity(ContactDto contactDto) {
+        return null;
+    }
+
+    @Override
+    public Contact convertToEntityNewContact(ContactDto contactDto) {
+        return null;
+    }
+
+    @Override
+    public ContactDetailsDto getContactDetails(String idContact) {
+        return null;
+    }
+}
