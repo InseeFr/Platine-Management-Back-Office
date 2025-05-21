9396469abe48c5dcfcb10594638c5fa25955c376
Betty Becuwe <betty.becuwe@insee.fr>
2025-05-21 14:42:10 +0200

    fix: changeset  name
    fix: changeset name

--- platine-management-db/src/main/resources/db/changelog/031_create-questioning-assistance-mail.sql
+++ platine-management-db/src/main/resources/db/changelog/031_create-questioning-assistance-mail.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
 
---changeset bettybecuwe:021_create-questioning-assistance-mail.sql
+--changeset bettybecuwe:031_create-questioning-assistance-mail.sql
 
 ALTER TABLE public.questioning add assistance_mail varchar NULL;
