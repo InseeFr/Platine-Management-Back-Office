e2b2486989326ad40344d75f9837dacee4ea8dbf
davdarras <david.darras@insee.fr>
2025-06-06 14:55:04 +0200

    feat: add multi criteria search

--- platine-management-shared/src/main/java/fr/insee/survey/datacollectionmanagement/query/dto/SearchQuestioningDto.java
+++ platine-management-shared/src/main/java/fr/insee/survey/datacollectionmanagement/query/dto/SearchQuestioningDto.java
@@ -1,22 +1,46 @@
 package fr.insee.survey.datacollectionmanagement.query.dto;
 
+import fr.insee.survey.datacollectionmanagement.questioning.enums.TypeCommunicationEvent;
+import fr.insee.survey.datacollectionmanagement.questioning.enums.TypeQuestioningEvent;
+import lombok.Getter;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public interface SearchQuestioningDto {
-    Long getQuestioningId();
+@Getter
+public class SearchQuestioningDto {
+    private final Long questioningId;
+    private final String campaignId;
+    private final TypeCommunicationEvent lastCommunicationType;
+    private final Date validationDate;
+    private final TypeQuestioningEvent highestEventType;
+    private final String surveyUnitId;
+    private final String identificationCode;
+    private final List<String> contactIds;
 
-    String getCampaignId();
+    public SearchQuestioningDto(Long questioningId,
+                                   String campaignId,
+                                   TypeCommunicationEvent lastCommunicationType,
+                                   Date validationDate,
+                                   TypeQuestioningEvent highestEventType,
+                                   String surveyUnitId,
+                                   String identificationCode,
+                                   String contactId) {
+        this.questioningId = questioningId;
+        this.campaignId = campaignId;
+        this.lastCommunicationType = lastCommunicationType;
+        this.validationDate = validationDate;
+        this.highestEventType = highestEventType;
+        this.surveyUnitId = surveyUnitId;
+        this.identificationCode = identificationCode;
+        this.contactIds = new ArrayList<>();
+        this.contactIds.add(contactId);
+    }
 
-    List<String> getListContactIdentifiers();
-
-    String getSurveyUnitId();
-
-    String getSurveyUnitIdentificationCode();
-
-    String getLastEvent();
-
-    String getLastCommunication();
-
-    Date getValidationDate();
+    public void addContactId(String contactId) {
+        if(!this.contactIds.contains(contactId)) {
+            this.contactIds.add(contactId);
+        }
+    }
 }
