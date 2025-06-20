024326dc27f0972f280f0cb64c634ce8f6b73bb6
Fabrice Bibonne <fabrice.bibonne@gmail.com>
2025-06-20 16:16:25 +0200

    Enhance database model for mail communications bis (#177)
    * feat (communication mail) add tables for sender
    
    * feat (communication mail) update liquibase
    
    * pom (minor upgrade)

---
+++ platine-management-db/src/main/resources/db/changelog/050_create-mail-communication-tables.sql
@@ -0,0 +1,17 @@
+alter table questioning_communication alter column id SET DEFAULT nextval('questioning_communication_seq');
+create table properties_resolver(id bigint GENERATED ALWAYS AS IDENTITY primary key, json_property_resolvers varchar);
+create table mail
+        (
+            id bigint GENERATED ALWAYS AS IDENTITY primary key,
+            questioning_id bigint not null references questioning(id),
+            template varchar not null,
+            mail_subject varchar,
+            properties_resolvers_id bigint references properties_resolver(id),
+            mail_state varchar,
+            to_send_at timestamp,
+            sent_at timestamp,
+            recipients varchar ARRAY,
+            constraint unik_questid_template unique(questioning_id, template, to_send_at)
+        );
+CREATE INDEX mail_questid_index ON mail USING btree (questioning_id);
+CREATE INDEX mail_propresolv_index ON mail USING btree (properties_resolvers_id);
