c9904b5ad36e26336d9d5261b478126768a7b2ac
farid.ait-karra <farid.ait-karra@insee.fr>
2025-06-30 13:54:39 +0200

    feat: add campagne constraints - 2

--- platine-management-db/src/main/resources/db/changelog/061_add-constraints-campagne.sql
+++ platine-management-db/src/main/resources/db/changelog/061_add-constraints-campagne.sql
@@ -2,5 +2,8 @@
 
 --changeset farid aitkarra:061
 
+UPDATE public.campaign SET sensitivity = false WHERE sensitivity IS NULL;
+UPDATE public.campaign SET datacollection_target = 'LUNATIC_NORMAL' WHERE datacollection_target IS NULL;
+
 ALTER TABLE public.campaign ALTER COLUMN sensitivity SET NOT NULL;
 ALTER TABLE public.campaign ALTER COLUMN datacollection_target SET NOT NULL;
