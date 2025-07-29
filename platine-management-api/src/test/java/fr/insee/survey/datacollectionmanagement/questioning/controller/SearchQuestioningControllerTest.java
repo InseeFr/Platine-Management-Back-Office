6c317a32695513a19c6d308b691d66c12d23580a
Mathieu Bergonzini <100563885+mbergonzini@users.noreply.github.com>
2025-07-29 15:03:55 +0200

    Feat add scores interrogations search (#208)

---
+++ platine-management-api/src/test/java/fr/insee/survey/datacollectionmanagement/questioning/controller/SearchQuestioningControllerTest.java
@@ -0,0 +1,127 @@
+package fr.insee.survey.datacollectionmanagement.questioning.controller;
+
+import fr.insee.survey.datacollectionmanagement.configuration.AuthenticationUserProvider;
+import fr.insee.survey.datacollectionmanagement.constants.AuthorityRoleEnum;
+import fr.insee.survey.datacollectionmanagement.constants.UrlConstants;
+import fr.insee.survey.datacollectionmanagement.questioning.service.QuestioningService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@ActiveProfiles("test")
+class SearchQuestioningControllerTest {
+
+    @Autowired
+    QuestioningService questioningService;
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void init() {
+        SecurityContextHolder.getContext().setAuthentication(AuthenticationUserProvider.getAuthenticatedUser("test", AuthorityRoleEnum.ADMIN));
+    }
+
+    @Test
+    void testSearchWithoutParams() throws Exception {
+        mockMvc.perform(post(UrlConstants.API_QUESTIONINGS_SEARCH)
+                        .content("{}")
+                        .param("page", "0")
+                        .param("pageSize", "20")
+                        .param("sortBy", "score")
+                        .param("sortDirection", "ASC")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", hasSize(10)))
+                .andExpect(jsonPath("$.number", is(0)))
+                .andExpect(jsonPath("$.size", is(20)))
+                .andExpect(jsonPath("$.sort.sorted", is(true)));
+    }
+
+    @Test
+    void testSearchWithParams() throws Exception {
+        String json = """
+                {
+                  "campaignIds": [
+                    "SOURCE12023T01"
+                  ]
+                }
+                """;
+        mockMvc.perform(post(UrlConstants.API_QUESTIONINGS_SEARCH)
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", hasSize(3)))
+                .andExpect(jsonPath("$.number", is(0)))
+                .andExpect(jsonPath("$.size", is(20)))
+                .andExpect(jsonPath("$.sort.sorted", is(false)));
+    }
+
+    @Test
+    void testPaginationAndSorting() throws Exception {
+        mockMvc.perform(post(UrlConstants.API_QUESTIONINGS_SEARCH)
+                        .content("{}")
+                        .param("page", "1")
+                        .param("pageSize", "5")
+                        .param("sortBy", "score")
+                        .param("sortDirection", "DESC")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.number", is(1)))
+                .andExpect(jsonPath("$.size", is(5)))
+                .andExpect(jsonPath("$.sort.sorted", is(true)));
+    }
+
+    @Test
+    void testSortParamsIncomplete() throws Exception {
+        mockMvc.perform(post(UrlConstants.API_QUESTIONINGS_SEARCH)
+                        .content("{}")
+                        .param("sortBy", "score")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.sort.sorted", is(false)));
+
+        mockMvc.perform(post(UrlConstants.API_QUESTIONINGS_SEARCH)
+                        .content("{}")
+                        .param("sortDirection", "ASC")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.sort.sorted", is(false)));
+    }
+
+    @Test
+    void testSearchWithResultBiggerThanPageSize() throws Exception {
+        mockMvc.perform(post(UrlConstants.API_QUESTIONINGS_SEARCH)
+                        .content("{}")
+                        .param("page", "0")
+                        .param("pageSize", "5")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", hasSize(5)))
+                .andExpect(jsonPath("$.number", is(0)))
+                .andExpect(jsonPath("$.size", is(5)))
+                .andExpect(jsonPath("$.last", is(false)));
+    }
+
+
+
+}
