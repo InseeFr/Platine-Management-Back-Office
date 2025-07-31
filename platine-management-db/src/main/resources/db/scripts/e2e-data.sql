0469549c485259e6d06b81f052b1ed2fa2a34c65
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2025-07-31 10:39:17 +0200

    feat: update tests + remove useless constraint

--- platine-management-db/src/main/resources/db/scripts/e2e-data.sql
+++ platine-management-db/src/main/resources/db/scripts/e2e-data.sql
@@ -6,8 +6,9 @@ VALUES
 ('SOURCE_1', 'Source 1 containing a campaign with one interrogation for a contact', false, 'X', 'One interrogation', 'HOUSEHOLD'),
 ('SOURCE_2', 'Source 2 containing a campaign with multiple interrogations and one UE for a contact', false, 'X', 'Multi-interrogations, one SU', 'BUSINESS'),
 ('SOURCE_3', 'Source 3 containing a campaign with multiple sources and UEs for a contact', false, 'X', 'Multi-interrogations, one SU', 'BUSINESS'),
-('SOURCE_4', 'Source 4 containing a campaign with multiple sources and UEs for a contact', false, 'X', 'Multi-SU', 'BUSINESS'),
-('SOURCE_EEC', 'Source EEC', false, 'X', 'EEC', 'HOUSEHOLD');
+('SOURCE_R2D2', 'Source R2D2 containing a campaign with multiple sources and UEs for a contact', false, 'X', 'Multi-SU R2D2', 'BUSINESS'),
+('eee', 'Source EEC', false, 'X', 'EEC', 'HOUSEHOLD'),
+('SOURCE_CLOSED', 'Source closed containing a closed campaign', false, 'X', 'Closed', 'HOUSEHOLD');
 
                                  -- E2E tests survey --
 
@@ -23,25 +24,30 @@ VALUES
        'This survey is used for E2E testing, it is inherited from SOURCE_3. It has a campaign with multiple interrogations and SUs', 'E2E Survey 3', 'http://notice/E2E_SURVEY_3', 0,
        'Short objectives for E2E_SURVEY_3', 'Short wording E2E_SURVEY_3', 'http://specimen/E2E_SURVEY_3',
        '2025xxxxx', 2025, 'SOURCE_3'),
-('E2E_SURVEY_4', 'http://cnis/E2E_SURVEY_4', '', 'http://diffusion/E2E_SURVEY_4',
-       'This survey is used for E2E testing, it is inherited from SOURCE_4. It has a campaign with multiple interrogations and SUs', 'E2E Survey 4', 'http://notice/E2E_SURVEY_4', 0,
-       'Short objectives for E2E_SURVEY_4', 'Short wording E2E_SURVEY_4', 'http://specimen/E2E_SURVEY_4',
-       '2025xxxxx', 2025, 'SOURCE_4'),
-('eec', 'http://cnis/E2E_SURVEY_EEC', '', 'http://diffusion/E2E_SURVEY_EEC',
-       'This survey is used for E2E testing, it is inherited from SOURCE_EEC', 'E2E Survey EEC', 'http://notice/E2E_SURVEY_EEC', 0,
+('E2E_SURVEY_R2D2', 'http://cnis/E2E_SURVEY_R2D2', '', 'http://diffusion/E2E_SURVEY_R2D2',
+       'This R2D2 survey is used for E2E testing, it is inherited from SOURCE_R2D2. It has a campaign with multiple interrogations and SUs', 'E2E Survey 4', 'http://notice/E2E_SURVEY_R2D2', 0,
+       'Short objectives for E2E_SURVEY_R2D2', 'Short wording E2E_SURVEY_R2D2', 'http://specimen/E2E_SURVEY_R2D2',
+       '2025xxxxx', 2025, 'SOURCE_R2D2'),
+('E2E_SURVEY_EEC', 'http://cnis/E2E_SURVEY_EEC', '', 'http://diffusion/E2E_SURVEY_EEC',
+       'This survey is used for E2E testing, it is inherited from eee', 'E2E Survey EEC', 'http://notice/E2E_SURVEY_EEC', 0,
        'Short objectives for E2E_SURVEY_EEC', 'Short wording E2E_SURVEY_EEC', 'http://specimen/E2E_SURVEY_EEC',
-       '2025xxxxx', 2025, 'SOURCE_EEC');
+       '2025xxxxx', 2025, 'eee'),
+('E2E_SURVEY_CLOSED', 'http://cnis/E2E_SURVEY_CLOSED', '', 'http://diffusion/E2E_SURVEY_CLOSED',
+       'This survey is used for E2E testing, it is inherited from SOURCE_CLOSED', 'E2E Survey CLOSED', 'http://notice/E2E_SURVEY_CLOSED', 0,
+       'Short objectives for E2E_SURVEY_CLOSED', 'Short wording E2E_SURVEY_CLOSED', 'http://specimen/E2E_SURVEY_CLOSED',
+       '2025xxxxx', 2025, 'SOURCE_CLOSED');
 
                                  -- E2E tests campaigns --
 
 INSERT INTO public.campaign
-(id, campaign_wording, survey_id, period_value, year_value, datacollection_target, sensitivity)
+(id, campaign_wording, survey_id, period_value, year_value, datacollection_target, sensitivity, operation_upload_reference)
 VALUES
-('E2E_CAMPAIGN_1', 'E2E Testing Campaign for a contact with only one interrogation existing in its partition', 'E2E_SURVEY_1', 'X00', 2025, 'LUNATIC_NORMAL', false),
-('E2E_CAMPAIGN_2', 'E2E Testing Campaign for a contact with multiple interrogations and one SU', 'E2E_SURVEY_2', 'X00', 2025, 'LUNATIC_NORMAL', false),
-('E2E_CAMPAIGN_3', 'E2E Testing Campaign for a contact with multiple interrogations SUs', 'E2E_SURVEY_3', 'X00', 2025, 'LUNATIC_NORMAL', false),
-('E2E_CAMPAIGN_4', 'E2E Testing Campaign for a contact with multiple interrogations SUs', 'E2E_SURVEY_4', 'X00', 2025, 'LUNATIC_NORMAL', false),
-('E2E_CAMPAIGN_EEC', 'E2E Testing Campaign EEC', 'eec', 'X00', 2025, 'LUNATIC_NORMAL', false);
+('E2E_CAMPAIGN_1', 'E2E Testing Campaign for a contact with only one interrogation existing in its partition', 'E2E_SURVEY_1', 'X00', 2025, 'LUNATIC_NORMAL', false, null),
+('E2E_CAMPAIGN_2', 'E2E Testing Campaign for a contact with multiple interrogations and one SU', 'E2E_SURVEY_2', 'X00', 2025, 'LUNATIC_NORMAL', false, null),
+('E2E_CAMPAIGN_3', 'E2E Testing Campaign for a contact with multiple interrogations SUs', 'E2E_SURVEY_3', 'X00', 2025, 'XFORM1', false, null),
+('E2E_CAMPAIGN_R2D2', 'E2E R2D2 Testing Campaign for a contact with multiple interrogations SUs', 'E2E_SURVEY_R2D2', 'X00', 2025, 'FILE_UPLOAD', false, 'op upload reference'),
+('E2E_CAMPAIGN_EEC', 'E2E Testing Campaign EEC', 'E2E_SURVEY_EEC', 'X00', 2025, 'LUNATIC_NORMAL', false, null),
+('E2E_CAMPAIGN_CLOSED', 'E2E Testing Campaign CLOSED', 'E2E_SURVEY_CLOSED', 'X00', 2025, 'LUNATIC_NORMAL', false, null);
 
                   -- E2E tests partitioning --
 
@@ -51,8 +57,10 @@ VALUES
 ('PART1', '2099-12-30 01:00:00.000', '2023-03-07 01:00:00.000', '2099-12-31 10:33:27.723',  'E2E_CAMPAIGN_1', 'E2E Partition 1 Label'),
 ('PART2', '2099-12-30 01:00:00.000', '2023-03-07 01:00:00.000', '2099-12-31 10:33:27.723',  'E2E_CAMPAIGN_2', 'E2E Partition 2 Label'),
 ('PART3', '2099-12-30 01:00:00.000', '2023-03-07 01:00:00.000', '2099-12-31 10:33:27.723',  'E2E_CAMPAIGN_3', 'E2E Partition 3 Label'),
-('PART4', '2099-12-30 01:00:00.000', '2023-03-07 01:00:00.000', '2099-12-31 10:33:27.723',  'E2E_CAMPAIGN_4', 'E2E Partition 4 Label'),
-('PARTEEC', '2099-12-30 01:00:00.000', '2023-03-07 01:00:00.000', '2099-12-31 10:33:27.723',  'E2E_CAMPAIGN_EEC', 'E2E Partition EEC Label');
+('PARTR2D2', '2099-12-30 01:00:00.000', '2023-03-07 01:00:00.000', '2099-12-31 10:33:27.723',  'E2E_CAMPAIGN_R2D2', 'E2E Partition R2D2 Label'),
+('PARTEEC', '2099-12-30 01:00:00.000', '2023-03-07 01:00:00.000', '2099-12-31 10:33:27.723',  'E2E_CAMPAIGN_EEC', 'E2E Partition EEC Label'),
+('PARTCLOSED', '2020-12-30 01:00:00.000', '2020-03-07 01:00:00.000', '2020-12-31 10:33:27.723',  'E2E_CAMPAIGN_CLOSED', 'E2E Partition CLOSED Label');
+
 
                     -- E2E tests adress --
 
@@ -69,7 +77,8 @@ VALUES
 ('E2E_RESPON_1',   'No comment', 'e2erespon1@insee.fr', 'Keith', '', 'Male', 'Cozart', '0600000000', 1, NULL, false,  NULL, NULL),
 ('E2E_RESPON_2',   'No comment', 'e2erespon2@insee.fr', 'Jaylah', 'CEO', 'Female', 'Hickmon', '0600000000', 1, NULL, false,  'Def Jam Records', NULL),
 ('E2E_RESPON_3',   'No comment', 'e2erespon3@insee.fr', 'Patrick Earl', 'CEO', 'Male', 'Houston', '0600000000', 1, NULL, false,  'Ghetty Green Inc.', NULL),
-('E2E_RESPON_EEC', 'No comment', 'e2erespon_eec@insee.fr', 'Nayvadius', '', 'Male', 'Cash', '0600000000', 1, NULL, false,  '', NULL);
+('E2E_RESPON_EEC', 'No comment', 'e2erespon_eec@insee.fr', 'Nayvadius', '', 'Male', 'Cash', '0600000000', 1, NULL, false,  '', NULL),
+('E2E_RESPON_CLOSED', 'No comment', 'e2erespon_closed@insee.fr', 'Skye', '', 'Female', 'Edwards', '0600000000', 1, NULL, false,  '', NULL);
 
                                  -- E2E tests contacts events --
 
@@ -79,7 +88,8 @@ VALUES
 (1, '2023-03-21 16:19:45.648', 1, 'E2E_RESPON_1', NULL),
 (2, '2023-03-21 16:19:45.648', 2, 'E2E_RESPON_2', NULL),
 (3, '2023-03-21 16:19:45.648', 3, 'E2E_RESPON_3', NULL),
-(4, '2023-03-21 16:19:45.648', 3, 'E2E_RESPON_EEC', NULL);
+(4, '2023-03-21 16:19:45.648', 3, 'E2E_RESPON_EEC', NULL),
+(5, '2023-03-21 16:19:45.648', 3, 'E2E_RESPON_CLOSED', NULL);
 
                                  -- E2E tests survey unit --
 
@@ -93,7 +103,9 @@ VALUES
 ('E2E_SU_5', 'Don Dada Records', '4', NULL, NULL),
 ('E2E_SU_6', 'Death Row Records', '5', NULL, NULL),
 ('E2E_SU_7', 'Aftermath Entertainment', '6', NULL, NULL),
-('E2E_SU_EEC', '', '', NULL, NULL);
+('E2E_SU_EEC', '', '', NULL, NULL),
+('E2E_SU_CLOSED', '', '', NULL, NULL);
+
 
                                 -- E2E tests questioning --
 
@@ -106,10 +118,12 @@ VALUES
 ('00000000-0000-0000-0000-000000000004', 4, 'PART3', 'Interrogation 4', 'E2E_SU_2'),
 ('00000000-0000-0000-0000-000000000005', 5, 'PART3', 'Interrogation 5', 'E2E_SU_3'),
 ('00000000-0000-0000-0000-000000000006', 6, 'PART3', 'Interrogation 6', 'E2E_SU_4'),
-('00000000-0000-0000-0000-000000000007', 7, 'PART4', 'Interrogation 7', 'E2E_SU_5'),
-('00000000-0000-0000-0000-000000000008', 8, 'PART4', 'Interrogation 8', 'E2E_SU_6'),
-('00000000-0000-0000-0000-000000000009', 9, 'PART4', 'Interrogation 9', 'E2E_SU_7'),
-('00000000-0000-0000-0000-000000000010', 10, 'PARTEEC', 'Interrogation EEC', 'E2E_SU_EEC');
+('00000000-0000-0000-0000-000000000007', 7, 'PARTR2D2', 'Interrogation 7', 'E2E_SU_5'),
+('00000000-0000-0000-0000-000000000008', 8, 'PARTR2D2', 'Interrogation 8', 'E2E_SU_6'),
+('00000000-0000-0000-0000-000000000009', 9, 'PARTR2D2', 'Interrogation 9', 'E2E_SU_7'),
+('00000000-0000-0000-0000-000000000010', 10, 'PARTEEC', 'Interrogation EEC', 'E2E_SU_EEC'),
+('00000000-0000-0000-0000-000000000011', 10, 'PARTCLOSED', 'Interrogation CLOSED', 'E2E_SU_EEC');
+
 
 
                                 -- E2E tests questioning accreditation --
@@ -126,7 +140,9 @@ VALUES
 ('00000000-0000-0000-0000-000000000007', 'platine-batch', '2025-03-08 15:49:51.670', 'E2E_RESPON_3', true, 7),
 ('00000000-0000-0000-0000-000000000008', 'platine-batch', '2025-03-08 15:49:51.670', 'E2E_RESPON_3', true, 8),
 ('00000000-0000-0000-0000-000000000009', 'platine-batch', '2025-03-08 15:49:51.670', 'E2E_RESPON_3', true, 9),
-('00000000-0000-0000-0000-000000000010', 'platine-batch', '2025-03-08 15:49:51.670', 'E2E_RESPON_EEC', true, 10);
+('00000000-0000-0000-0000-000000000010', 'platine-batch', '2025-03-08 15:49:51.670', 'E2E_RESPON_EEC', true, 10),
+('00000000-0000-0000-0000-000000000011', 'platine-batch', '2020-03-08 15:49:51.670', 'E2E_RESPON_CLOSED', true, 11);
+
 
                                 -- E2E tests questioning event --
 
@@ -142,7 +158,9 @@ VALUES
 (7,  '2025-01-11 11:05:07.380', 'INITLA',     '00000000-0000-0000-0000-000000000007', '{}'::jsonb, NULL),
 (8,  '2025-01-11 11:05:07.380', 'PARTIELINT', '00000000-0000-0000-0000-000000000008', '{}'::jsonb, NULL),
 (9,  '2025-01-11 11:05:07.380', 'REFUSAL',    '00000000-0000-0000-0000-000000000009', '{}'::jsonb, NULL),
-(10, '2025-01-11 11:05:07.380', 'PARTIELINT', '00000000-0000-0000-0000-000000000010', '{}'::jsonb, NULL);
+(10, '2025-01-11 11:05:07.380', 'PARTIELINT', '00000000-0000-0000-0000-000000000010', '{}'::jsonb, NULL),
+(11, '2025-01-11 11:05:07.380', 'PARTIELINT', '00000000-0000-0000-0000-000000000011', '{}'::jsonb, NULL);
+
 
                                 -- E2E tests views --
 
@@ -155,7 +173,8 @@ VALUES
 (4, 'E2E_CAMPAIGN_3', 'E2E_SU_2', 'E2E_RESPON_3'),
 (5, 'E2E_CAMPAIGN_3', 'E2E_SU_3', 'E2E_RESPON_3'),
 (6, 'E2E_CAMPAIGN_3', 'E2E_SU_4', 'E2E_RESPON_3'),
-(7, 'E2E_CAMPAIGN_4', 'E2E_SU_5', 'E2E_RESPON_3'),
-(8, 'E2E_CAMPAIGN_4', 'E2E_SU_6', 'E2E_RESPON_3'),
-(9, 'E2E_CAMPAIGN_4', 'E2E_SU_7', 'E2E_RESPON_3'),
-(10, 'E2E_CAMPAIGN_EEC', 'E2E_SU_EEC', 'E2E_RESPON_EEC');
+(7, 'E2E_CAMPAIGN_R2D2', 'E2E_SU_5', 'E2E_RESPON_3'),
+(8, 'E2E_CAMPAIGN_R2D2', 'E2E_SU_6', 'E2E_RESPON_3'),
+(9, 'E2E_CAMPAIGN_R2D2', 'E2E_SU_7', 'E2E_RESPON_3'),
+(10, 'E2E_CAMPAIGN_EEC', 'E2E_SU_EEC', 'E2E_RESPON_EEC'),
+(11, 'E2E_CAMPAIGN_CLOSED', 'E2E_SU_CLOSED', 'E2E_RESPON_CLOSED');
