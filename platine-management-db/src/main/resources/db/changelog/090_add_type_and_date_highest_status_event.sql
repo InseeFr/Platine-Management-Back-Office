3d16a4ba5559cf972e9ed9b14da3d995f0713eed
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2025-07-31 18:30:46 +0200

    refactor: rename

--- platine-management-db/src/main/resources/db/changelog/090_add_type_and_date_highest_status_event.sql
+++ platine-management-db/src/main/resources/db/changelog/090_add_type_and_date_highest_status_event.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset j75xzr:080_add_type_and_date_highest_status_event
+--changeset j75xzr:090_add_type_and_date_highest_status_event.sql
 
 ALTER TABLE questioning
     ADD COLUMN highest_type_event varchar,
