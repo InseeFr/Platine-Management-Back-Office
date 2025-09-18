a79d53a73c56de7684691d413984c4a3c6388998
PGPU0I <camille.corbin@insee.fr>
2025-09-18 09:48:57 +0200

    fix: normalize gender values to uppercase enum

--- platine-management-db/src/main/resources/db/changelog/092_fix_null_gender.sql
+++ platine-management-db/src/main/resources/db/changelog/092_fix_null_gender.sql
@@ -4,5 +4,10 @@
 
 -- Mettre à jour toutes les valeurs NULL existantes
 UPDATE public.contact
-SET gender = 'Undefined'
+SET gender = 'UNDEFINED'
 WHERE gender IS NULL;
+
+-- Harmoniser les anciennes valeurs avec la nouvelle convention en majuscules
+UPDATE public.contact SET gender = 'FEMALE' WHERE gender ILIKE 'female';
+UPDATE public.contact SET gender = 'MALE' WHERE gender ILIKE 'male';
+UPDATE public.contact SET gender = 'UNDEFINED' WHERE gender ILIKE 'undefined';
