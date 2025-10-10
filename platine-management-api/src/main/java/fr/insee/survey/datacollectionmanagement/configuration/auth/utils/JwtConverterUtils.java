007bef6eb8e797526d10f83369a10d84e632ef1f
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2025-10-10 09:49:25 +0200

    fix: sonar JwtConverterUtils constructor

--- platine-management-api/src/main/java/fr/insee/survey/datacollectionmanagement/configuration/auth/utils/JwtConverterUtils.java
+++ platine-management-api/src/main/java/fr/insee/survey/datacollectionmanagement/configuration/auth/utils/JwtConverterUtils.java
@@ -2,7 +2,6 @@ package fr.insee.survey.datacollectionmanagement.configuration.auth.utils;
 
 import fr.insee.survey.datacollectionmanagement.configuration.ApplicationConfig;
 import fr.insee.survey.datacollectionmanagement.constants.AuthorityRoleEnum;
-import lombok.NoArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
@@ -10,8 +9,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-@NoArgsConstructor
 public class JwtConverterUtils {
+
+    private JwtConverterUtils() {
+        throw new IllegalStateException("JwtConverterUtils can't be instantiated");
+    }
+
     public static void fillGrantedRoles(List<String> configRoles, AuthorityRoleEnum authorityRole, Map<String, List<SimpleGrantedAuthority>>  roles) {
         if(configRoles == null) {
             return;
