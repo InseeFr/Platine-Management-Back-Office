bc7b65233b0f3a17b9421e77336d334346f5eb87
Betty Becuwe <betty.becuwe@insee.fr>
2025-10-14 15:30:26 +0200

    fix: error

--- platine-management-db/src/main/resources/db/changelog/090_add_type_and_date_highest_status_event.sql
+++ platine-management-db/src/main/resources/db/changelog/090_add_type_and_date_highest_status_event.sql
@@ -5,24 +5,3 @@
 ALTER TABLE questioning
     ADD COLUMN highest_event_type varchar,
     ADD COLUMN highest_event_date timestamp;
-
-    ALTER TABLE questioning_communication
-        ADD COLUMN with_receipt boolean,
-        ADD COLUMN with_questionnaire boolean;
-
-    update questioning_communication qc
-    set with_receipt = false, with_questionnaire=false;.alter
-
-    update questioning_communication qc
-    set with_receipt = true
-    where qc."type" = 'COURRIER_CNRAR' or qc."type" ='COURRIER_MEDAR';
-
-
-
-    update questioning_communication qc
-    set "type" = 'COURRIER_CNR'
-    where qc."type" = 'COURRIER_CNRAR';
-
-    update questioning_communication qc
-    set "type" = 'COURRIER_MED'
-    where qc."type" = 'COURRIER_MEDAR';
