ca078ecbe9190aaecf6619db5178475c0edcec57
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2025-10-14 13:48:55 +0200

    feat: get support by source

--- platine-management-api/src/test/java/fr/insee/survey/datacollectionmanagement/dataloader/DataloaderTest.java
+++ platine-management-api/src/test/java/fr/insee/survey/datacollectionmanagement/dataloader/DataloaderTest.java
@@ -246,6 +246,11 @@ public class DataloaderTest {
         ownerInsee.setLabel("Insee");
         Set<Source> setSourcesInsee = new HashSet<>();
 
+        Support supportInsee = new Support();
+        supportInsee.setId("SupportInsee");
+        supportInsee.setLabel("Support INSEE");
+        supportInsee.setMail("mail.test@test.fr");
+
         while (sourceRepository.count() < 2) {
 
             Source source = new Source();
@@ -327,6 +332,8 @@ public class DataloaderTest {
                 source.setSurveys(setSurveys);
                 ownerRepository.saveAll(List.of(ownerInsee));
                 source.setOwner(ownerInsee);
+                supportRepository.save(supportInsee);
+                source.setSupport(supportInsee);
                 sourceRepository.save(source);
                 log.info("Source created : " + source);
                 ownerInsee.setSources(setSourcesInsee);
