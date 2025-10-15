ea1460e5b7b2bad91c6c5f1268a80fa128f59ca1
Betty Becuwe <betty.becuwe@insee.fr>
2025-10-15 14:42:05 +0200

    fix: limit privileges to put metadata to admin or weblient

--- platine-management-api/src/main/java/fr/insee/survey/datacollectionmanagement/configuration/auth/user/AuthorityPrivileges.java
+++ platine-management-api/src/main/java/fr/insee/survey/datacollectionmanagement/configuration/auth/user/AuthorityPrivileges.java
@@ -7,6 +7,7 @@ public class AuthorityPrivileges {
 
     public static final String HAS_MANAGEMENT_PRIVILEGES = "hasAnyRole('INTERNAL_USER', 'WEB_CLIENT', 'ADMIN')";
     public static final String HAS_ADMIN_PRIVILEGES = "hasRole('ADMIN')";
+    public static final String HAS_WEBCLIENT_PRIVILEGES = "hasAnyRole('WEB_CLIENT', 'ADMIN')";
     public static final String HAS_READER_PRIVILEGES = "hasAnyRole('INTERNAL_USER', 'WEB_CLIENT', 'ADMIN', 'READER')";
     public static final String HAS_RESPONDENT_PRIVILEGES = "hasRole('RESPONDENT')";
     public static final String HAS_PORTAL_PRIVILEGES = "hasAnyRole('PORTAL', 'INTERNAL_USER', 'WEB_CLIENT', 'ADMIN')";
