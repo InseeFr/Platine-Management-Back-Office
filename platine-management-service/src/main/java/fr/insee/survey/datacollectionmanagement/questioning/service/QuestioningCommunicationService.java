65683950383c96daff73cc76fe2e9a29faae6062
Betty Becuwe <77614323+BettyB979@users.noreply.github.com>
2025-10-15 11:47:54 +0200

    feat: delete communication PND when event deleted in moog (#260)
    * feat: delete communication PND when event deleted in moog
    
    * build: version 4.22.0

--- platine-management-service/src/main/java/fr/insee/survey/datacollectionmanagement/questioning/service/QuestioningCommunicationService.java
+++ platine-management-service/src/main/java/fr/insee/survey/datacollectionmanagement/questioning/service/QuestioningCommunicationService.java
@@ -2,9 +2,10 @@ package fr.insee.survey.datacollectionmanagement.questioning.service;
 
 import fr.insee.survey.datacollectionmanagement.questioning.dto.QuestioningCommunicationDto;
 import fr.insee.survey.datacollectionmanagement.questioning.dto.QuestioningCommunicationInputDto;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.UUID;
-import org.springframework.stereotype.Service;
 
 @Service
 public interface QuestioningCommunicationService {
@@ -12,5 +13,8 @@ public interface QuestioningCommunicationService {
     List<QuestioningCommunicationDto> findQuestioningCommunicationsByQuestioningId(UUID questioningId);
 
     void postQuestioningCommunication(QuestioningCommunicationInputDto questioningCommunicationInputDto);
+
+    void deleteQuestioningCommunication(Long questioningCommunicationId);
+
 
 }
