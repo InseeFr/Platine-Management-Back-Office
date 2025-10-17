c31245a9ba5b62c81a4809f9e5ad889311075c9b
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2025-10-17 12:15:16 +0200

    fix: checkmarx

--- platine-management-api/src/main/java/fr/insee/survey/datacollectionmanagement/questioning/controller/SearchSurveyUnitController.java
+++ platine-management-api/src/main/java/fr/insee/survey/datacollectionmanagement/questioning/controller/SearchSurveyUnitController.java
@@ -22,6 +22,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,6 +39,7 @@ import java.util.Set;
 @Tag(name = "3 - Survey units", description = "Enpoints to create, update, delete and find survey units")
 @Slf4j
 @RequiredArgsConstructor
+@Validated
 public class SearchSurveyUnitController {
 
 
