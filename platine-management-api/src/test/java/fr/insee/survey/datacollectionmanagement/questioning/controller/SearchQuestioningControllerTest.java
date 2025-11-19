8346fb7cd19afad8ca853f6d56b937fbd1640bd6
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2025-11-19 15:46:52 +0100

    fix: sonar issue

--- platine-management-api/src/test/java/fr/insee/survey/datacollectionmanagement/questioning/controller/SearchQuestioningControllerTest.java
+++ platine-management-api/src/test/java/fr/insee/survey/datacollectionmanagement/questioning/controller/SearchQuestioningControllerTest.java
@@ -16,7 +16,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ActiveProfiles;
@@ -35,8 +34,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 class SearchQuestioningControllerTest {
 
-    @Autowired
-    QuestioningService questioningService;
     @Autowired
     MockMvc mockMvc;
 
