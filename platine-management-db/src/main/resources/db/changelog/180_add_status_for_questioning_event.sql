7533747a78044c42396e777c8b1c6539869055fa
Camille CORBIN <camille.corbin@insee.fr>
2026-01-09 10:28:36 +0100

    fix: status for questioning event (#305)

--- platine-management-db/src/main/resources/db/changelog/180_add_status_for_questioning_event.sql
+++ platine-management-db/src/main/resources/db/changelog/180_add_status_for_questioning_event.sql
@@ -5,8 +5,5 @@ ALTER TABLE questioning_event
     ADD COLUMN IF NOT EXISTS status VARCHAR(255) DEFAULT 'AUTOMATIC';
 
 UPDATE questioning_event
-SET status = CASE
-                 WHEN type IN ('HC', 'WASTE', 'REFUSAL', 'VALPAP') THEN 'MANUAL'
-                 ELSE 'AUTOMATIC'
-    END
-WHERE status IS NULL;
+SET status = 'MANUAL'
+WHERE type IN ('HC', 'WASTE', 'REFUSAL', 'VALPAP');
